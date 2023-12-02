bc6983a4e53e7026094009e9becb633359c577f8
YGsDarius <taokhongpk9b@gmail.com>
2023-12-02 10:43:29 +0700

    go

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -10,11 +10,11 @@ const Navbar = () => {
             <motion.span 
             initial= {{ opacity: 0, scale: 0.5 }}
             animate= {{ opacity: 1, scale: 1 }}
-            transition= {{ transition: 0.5 }}
+            transition= {{ duration: 0.5 }}
             >
             Nguyen Bao Trung</motion.span>
             <div className="social">
-                <a href=""><img src="/facebook.png" alt="" /></a>
+                <a href="https://www.facebook.com/TrungYT3645" target="_blank"><img src="/facebook.png" alt="" /></a>
                 <a href=""><img src="/instagram.png" alt="" /></a>
                 <a href=""><img src="/youtube.png" alt="" /></a>
             </div>
