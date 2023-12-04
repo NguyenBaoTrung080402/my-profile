fc133abefdb880e6ad679d42551ac3dbac2f3e29
YGsDarius <taokhongpk9b@gmail.com>
2023-12-05 00:10:01 +0700

    fix path

--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -7,28 +7,28 @@ const items = [
         id: 1,
         title: "Sapphire Sands",
         date: "8-May-2023 -- 12-July-2023",
-        img: "../../../public/SAPPHIRE SANDS RESORT.png",
+        img: "/SAPPHIRE SANDS RESORT.png",
         desc: "Đây là một dự án đầu tay về xử dụng về react.js, java spring boot, mySQL. Với sự tham gia của 5 thành viên để phát triển, dự án này mang đến cho người dùng, trải nghiệm 1 hệ thống đặt phòng dành riêng cho khách sạn với các dịch vụ đi kèm."
     },
     {
         id: 2,
         title: "JOT",
         date: "8-Jully-2023 -- 29-Sep-2023",
-        img: "../../../public/Jot.png",
+        img: "/Jot.png",
         desc: "JOT(Job Opportunity) là dự án, làm về 1 trang web về tìm việc làm sử dụng Java Spring Boot, vue.js , mySQL để phát triển, với sự tham gia phát triển gồm 5 thành viên. Hệ thống tìm việc giúp đơn giản hóa quá trình đăng ký và theo dõi tiến trình tìm việc, cung cấp thông tin chi tiết về các công ty và vị trí tuyển dụng."
     },
     {
         id: 3,
         title: "Manager School",
         date: "10-Oct-2023 -- 26-Nov-2023",
-        img: "../../../public/manager-bg.png",
+        img: "/manager-bg.png",
         desc: "Manager School là 1 dự án phát triển hệ thống quản lý 1 trường học, sử dụng Java Spring Boot, Vue.js, MySQL, IoT để phát triển, với sự tham gia phát triển gồm 3 thành viên. Dự án quản lý trường học là một hệ thống toàn diện được thiết kế để cải thiện hiệu suất và quản lý các hoạt động trong môi trường giáo dục."
     },
     {
         id: 4,
         title: "Cosmetics",
         date: "20-Oct-2023 -- 1-Jan-2024",
-        img: "../../../public/cosmetics-logo.png",
+        img: "/cosmetics-logo.png",
         desc: "Cosmetics là 1 dự án đang được phát triển bởi đội ngũ, được sử dụng React.js, MySQL, Java Spring Boot và AI, dự án tham gia gồm 5 thành viên. Cosmetics hứa hẹn sẽ là 1 sàn thương mại giúp nhiều nhà cung cấp sản phẩm có thể trao đổi mua bán 1 cách chính thống. "
     }
 ]
